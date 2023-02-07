9dd1bea9474d2375b1eb7983c7be3bdca4abc030
cbobio <93291316+cbobio@users.noreply.github.com>
2023-02-07 13:47:07 -0300

    banco em SQL

---
+++ Docker: Utilização Prática no Cenário de Microsserviços/banco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dados(
+    AlunoID int(20) not null,
+    Nome varchar (50) not null,
+    Sobrenome varchar (50) not null,
+    Endereco varchar (50) not null,
+    Cidade varchar (50) not null,
+    Host varchar (50) not null,
+    primary key (AlunoID)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
